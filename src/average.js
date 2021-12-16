1bc677f772a07284bd9e09542d6d3f6391552351
leoff00 <leof0180@gmail.com>
2021-12-16 01:05:42 -0300

    initial commit

--- src/average.js
+++ src/average.js
@@ -1,14 +1,15 @@
 module.exports = function averageCalc(...notes) {
-  if (!notes)
-    return new Error(`Please, enter with the args length -> ${notes.length}`);
-
   notes.map((e) => {
     return typeof e !== "number" ? new Error("Invalid data") : e;
   });
 
   const total = Math.round(
     notes.reduce((acc, curr) => acc + curr, 0) / notes.length
-  ).toFixed(1);
+  );
 
-  return total;
+  const validate = notes.map((e) => {
+    return !e ? [] : e;
+  });
+
+  return total || validate;
 };
--- src/average.spec.js
+++ src/average.spec.js
@@ -1,13 +1,15 @@
 const averageCalc = require("./average");
 
-test("The average calculator should be 9", () => {
-  expect(averageCalc(10, 9, 9)).toBe(9);
+test("The average calculator should be 10", () => {
+  const n = null;
+  expect(averageCalc(10, 10, 10)).toEqual(10);
+  expect(averageCalc(10, 10, 10)).not.toBe(n);
 });
 
-test("The average calculator should be an error", () => {
-  expect(averageCalc("foo", "bar", "abc")).toBe("Invalid Data");
+test("The average calculator should be a number", () => {
+  expect(averageCalc("foo", "bar", "abc")).not.toBe(typeof Number);
 });
 
-test("The average calculator should request for arguments", () => {
-  expect(averageCalc()).toBe("Please, enter with the args length");
+test("The average calculator should not be an empty array ", () => {
+  expect(averageCalc()).toStrictEqual([]);
 });
